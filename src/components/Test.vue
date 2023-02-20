61b72f68f257f6cb9bfdbc485bcbd09231248e47
Sangjin-Mac <mitotero@kakao.com>
2023-02-20 14:07:58 +0900

    작품 이미지 변경

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,5 +1,114 @@
 <template>
+    <div class="row Artworks-Div-Bottom">
+        <!-- Filter by -->
+        <div class="col-12" id="Mobile-Filter">
+            <div class="imgFilter-Div">
+                <div class="row mb-2 text-center">Filter by</div>
+                <div class="mt-4 text-start FilterTime-Div">Time Period</div>
 
+                <div class="row">
+                    <form class="box">
+                        <!-- <RangeSlider v-model="imgRange" bar-color="#bebefe" :min="imgYear.Min" :max="imgYear.Max"
+                                    :keep-just-significant-figures="false" id="timePeriod">
+                                </RangeSlider> -->
+                        <RangeSlider v-model="imgRange" bar-color="#bebebe" :min="imgYear.Min" :max="imgYear.Max"
+                            :keep-just-significant-figures="false" @update:model-value="imgYearRange(selectClass)"
+                            id="timePeriod">
+                        </RangeSlider>
+                    </form>
+                </div>
+
+                <div class="mt-4 text-start FilterTime-Div">Size</div>
+                <div class="row mt-2">
+                    <form class="box">
+                        <input type="range" v-model="rangeImg.W" id="rangeWidth" :min="0" :max="imgSizeFixed.W"
+                            @input="imgSizeRange(selectClass)" />
+                        <input type="range" v-model="rangeImg.H" id="rangeHeight" :min="0" :max="imgSizeFixed.W"
+                            @input="imgSizeRange(selectClass)" />
+                        <div :style="{ width: (sizePeriodGuideW / 1.8) + '%', height: (sizePeriodGuideH * 1.94) + '%' }"
+                            class="img-Box">
+                        </div>
+                    </form>
+                </div>
+                <div id="Size-Box">
+                    <div class="row mt-3">
+                        {{ rangeImg.W }}cm X {{ rangeImg.H }}cm
+                    </div>
+                    <div class="row">
+                        {{ parseInt(rangeImg.W * 0.393701) }}inch X {{ parseInt(rangeImg.H * 0.393701) }}inch
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Filter by -->
+        <div class="col-md-2 col-lg-2 mt-5" id="nonMobile-Filter">
+            <div class="imgFilter-Div">
+                <div class="row mb-2 text-center">Filter by</div>
+                <div class="mt-4 text-start FilterTime-Div">Time Period</div>
+
+                <div class="row">
+                    <form class="box">
+                        <!-- <RangeSlider v-model="imgRange" bar-color="#bebefe" :min="imgYear.Min" :max="imgYear.Max"
+                                    :keep-just-significant-figures="false" id="timePeriod">
+                                </RangeSlider> -->
+                        <RangeSlider v-model="imgRange" bar-color="#bebebe" :min="imgYear.Min" :max="imgYear.Max"
+                            :keep-just-significant-figures="false" @update:model-value="imgYearRange(selectClass)"
+                            id="timePeriod">
+                        </RangeSlider>
+                    </form>
+                </div>
+
+                <div class="mt-4 text-start FilterTime-Div">Size</div>
+                <div class="row mt-2">
+                    <form class="box">
+                        <input type="range" v-model="rangeImg.W" id="rangeWidth" :min="0" :max="imgSizeFixed.W"
+                            @input="imgSizeRange(selectClass)" />
+                        <input type="range" v-model="rangeImg.H" id="rangeHeight" :min="0" :max="imgSizeFixed.W"
+                            @input="imgSizeRange(selectClass)" />
+                        <div :style="{ width: (sizePeriodGuideW / 1.8) + '%', height: (sizePeriodGuideH * 1.94) + '%' }"
+                            class="img-Box">
+                        </div>
+                    </form>
+                </div>
+                <div id="Size-Box">
+                    <div class="row mt-3">
+                        {{ rangeImg.W }}cm X {{ rangeImg.H }}cm
+                    </div>
+                    <div class="row">
+                        {{ parseInt(rangeImg.W * 0.393701) }}inch X {{ parseInt(rangeImg.H * 0.393701) }}inch
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Img List -->
+        <div class="sol-sm-12 col-md-10 col-lg-10">
+            <div class="row">
+                <h4 class="text-start mb-4">
+                    {{ (imgData.length).toLocaleString('kr') }} {{ selectClass }} works
+                </h4>
+                <hr />
+            </div>
+
+            <div class="row mt-4">
+                <!-- <div class="row d-flex align-items-end"> -->
+                <div class="row">
+                    <masonry-wall :items="imgList" :ssr-columns="1" :column-width="380" :gap="40">
+                        <template #default="{ item, index }">
+                            <div class="mb-4" @click="$router.push('/artworks/' + imgData[index].imgID)">
+                                <img class="img-fluid mb-1" :src="'/img/Artworks/' + item[0]" />
+                                <div class="imgTilteFont text-start">{{ imgData[index].imgTitle }}</div>
+                            </div>
+                        </template>
+                    </masonry-wall>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
 </template>
 <script setup>
 
